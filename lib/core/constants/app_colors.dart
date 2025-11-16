3789f39aa5643389757f8bcae86eb7f1ff376716
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-11-16 11:35:51 +0500

    code added to app_colors and main.dart about the theme of the whole app

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFF47430);
+  static const Color gold = Color(0xFFF6B409);
+  static const Color background = Color(0xFF0F0F0F);
+  static const Color card = Color(0xFF1A1A1A);
+  static const Color white = Colors.white;
+  static const Color dimWhite = Colors.white70;
+}
